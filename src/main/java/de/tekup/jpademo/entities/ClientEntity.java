0499490a402b3f0ec6406ea89a62e0e3304200d0
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2022-10-10 11:05:47 +0100

    change class with lombok

--- src/main/java/de/tekup/jpademo/entities/ClientEntity.java
+++ src/main/java/de/tekup/jpademo/entities/ClientEntity.java
@@ -1,9 +1,12 @@
 package de.tekup.jpademo.entities;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "client")
+@Data
 public class ClientEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,45 +19,6 @@ public class ClientEntity {
     private String prenom;
     private String adress;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Integer getCin() {
-        return cin;
-    }
-
-    public void setCin(Integer cin) {
-        this.cin = cin;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public String getAdress() {
-        return adress;
-    }
-
-    public void setAdress(String adress) {
-        this.adress = adress;
-    }
 
 
 
