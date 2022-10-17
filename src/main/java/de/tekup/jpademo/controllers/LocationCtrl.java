6000e5bc2431c238c2da57ee9a461359b81d4a07
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2022-10-17 11:48:17 +0100

    Add Voiture to save location

--- src/main/java/de/tekup/jpademo/controllers/LocationCtrl.java
+++ src/main/java/de/tekup/jpademo/controllers/LocationCtrl.java
@@ -2,6 +2,7 @@ package de.tekup.jpademo.controllers;
 
 import de.tekup.jpademo.entities.LocationEntity;
 import de.tekup.jpademo.services.LocationService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -9,15 +10,16 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@AllArgsConstructor
 public class LocationCtrl {
-    @Autowired
+
     private LocationService locationService;
 
-    @PostMapping("/locations/add/client/{idClient}")
+    @PostMapping("/locations/add/client/{idClient}/voiture/{idVoiture}")
     @ResponseBody
     public LocationEntity addLocation(@RequestBody LocationEntity location,
-                              @PathVariable("idClient") int idClient){
-        return locationService.saveLocation(location,idClient);
+                              @PathVariable("idClient") int idClient, @PathVariable("idVoiture") int idVoiture){
+        return locationService.saveLocation(location,idClient, idVoiture);
     }
 
     @GetMapping("/locations")
--- src/main/java/de/tekup/jpademo/services/LocationService.java
+++ src/main/java/de/tekup/jpademo/services/LocationService.java
@@ -2,6 +2,7 @@ package de.tekup.jpademo.services;
 
 import de.tekup.jpademo.entities.ClientEntity;
 import de.tekup.jpademo.entities.LocationEntity;
+import de.tekup.jpademo.entities.VoitureEntity;
 import de.tekup.jpademo.repos.LocationRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,12 +16,16 @@ public class LocationService {
     private LocationRepository locationRepository;
 
     private ClientService clientService;
+    private VoitureService voitureService;
 
-    public LocationEntity saveLocation(LocationEntity location,int idClient){
+    public LocationEntity saveLocation(LocationEntity location,int idClient,int idVoiture){
         //search for client in DB
         ClientEntity client = clientService.getClientById(idClient);
-        // add client to location object
+        //Search for voiture in DB
+        VoitureEntity voiture = voitureService.getVoitureById(idVoiture);
+        // add client and voiture to location object
         location.setClient(client);
+        location.setVoiture(voiture);
         //save location in DB
         return locationRepository.save(location);
 
