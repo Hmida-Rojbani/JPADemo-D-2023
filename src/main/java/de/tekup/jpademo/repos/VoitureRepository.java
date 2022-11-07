12fc7f07ed01d1e91985208843a190d7a1733a33
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2022-11-07 13:37:12 +0100

    Add prix calculation

--- src/main/java/de/tekup/jpademo/repos/VoitureRepository.java
+++
@@ -1,7 +0,0 @@
-package de.tekup.jpademo.repos;
-
-import de.tekup.jpademo.entities.VoitureEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VoitureRepository extends CrudRepository<VoitureEntity,Integer> {
-}
