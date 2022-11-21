7fcde4aa582fda4abdf669603c96a90f35b883b2
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2022-11-21 15:50:39 +0100

    display Voitures from DB

--- src/main/java/de/tekup/jpademo/controllers/ViewVoitureCtrl.java
+++ src/main/java/de/tekup/jpademo/controllers/ViewVoitureCtrl.java
@@ -1,7 +1,9 @@
 package de.tekup.jpademo.controllers;
 
+import de.tekup.jpademo.services.VoitureService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,9 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/voitures/ui")
 @AllArgsConstructor
 public class ViewVoitureCtrl {
-
+    private VoitureService voitureService;
     @GetMapping("/")
-    public String displayVoitures(){
+    public String displayAllCars(Model model){
+        model.addAttribute("voitures",voitureService.getAllVoitures());
         return "car";
     }
 }
