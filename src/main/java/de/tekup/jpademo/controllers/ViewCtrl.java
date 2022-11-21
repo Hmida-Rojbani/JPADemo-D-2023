8de4540460f9122830cf74391fe1e80d94c204b2
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2022-11-21 15:22:52 +0100

    Handle Errors and Exception

--- src/main/java/de/tekup/jpademo/controllers/ViewCtrl.java
+++ src/main/java/de/tekup/jpademo/controllers/ViewCtrl.java
@@ -1,14 +1,19 @@
 package de.tekup.jpademo.controllers;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @Controller
-public class ViewCtrl {
+public class ViewCtrl implements ErrorController {
     @GetMapping({"/","/index"})
     public String landingPage(Model model){
         String today = LocalDate.now()
@@ -20,5 +25,19 @@ public class ViewCtrl {
     @GetMapping("/about")
     public String aboutPage(){
         return "about";
+    }
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error-404";
+            }
+        }
+        return "error";
     }
 }
